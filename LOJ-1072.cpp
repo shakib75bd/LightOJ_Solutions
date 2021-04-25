d14f04e95be50447bc7fd9e1d6e1495445776006
shakib75bd <59929433+shakib75bd@users.noreply.github.com>
2021-04-25 22:21:05 +0600

    Create LOJ-1072.cpp

---
+++ LOJ-1072.cpp
@@ -0,0 +1,52 @@
+//## Shakib Hossen Shanto
+//## CSE,BRUR -|- April 2021
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+#define frn(n)      for(ll i=0;i<n;i++)
+#define flp(a,b)    for(ll i=a;i<=b;i++)
+#define test        ll tes; cin>>tes; while(tes--)
+#define nn          "\n"
+#define yes         cout<<"Yes\n"
+#define no          cout<<"No\n"
+
+// Well, Firstly we need to find out the angel for every small ball
+// I mean the angle of the small arcs.
+// Then we can easily divide it into two arcs and by adding a vertical
+// height . Then, we just need to imagine a Right angled triangle
+// and apply its rule with sin. Thats all
+
+
+void solve()
+{
+    ll cas=1;
+    test{
+        cout<<"Case "<<cas<<": ";
+        cas++;
+
+        double big_r,n;
+        cin>>big_r>>n;
+
+        double theta = (360/n)/2;
+        theta = (M_PI*theta)/180;
+        theta = sin(theta);
+        //cout<<theta<<nn;
+        double ans = (big_r * theta) / (1 + theta);
+
+        cout<<fixed<<setprecision(10)<<ans<<nn;
+    }
+}
+
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    	freopen("input.txt", "r", stdin);
+    	freopen("output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    solve();    return 0;
+}
